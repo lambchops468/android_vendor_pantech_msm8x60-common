42dda59d18e55dc380dfc925240d5c6580e40811
M. Ignacio Fernández López <mifl.pc@gmail.com>
2014-07-18 21:38:34 +0200

    qcom-common: fix errors in paths.
    Replace :/system by :system. This error is not allowed to put the files in the build directory.
    
    Change-Id: I10674f104768849e6cdd9773af40d2c948aeca3d

--- qcom-common-vendor-blobs.mk
+++ qcom-common-vendor-blobs.mk
@@ -31,25 +31,25 @@ PRODUCT_COPY_FILES += \
 
 # All the blobs necessary for adreno
 PRODUCT_COPY_FILES += \
-    $(VENDOR_DEVICE_FOLDER)/system/app/ProfilerPlaybackTools.apk:/system/app/ProfilerPlaybackTools.apk \
-    $(VENDOR_DEVICE_FOLDER)/system/bin/gpu_dcvsd:/system/bin/gpu_dcvsd \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a300_pfp.fw:/system/etc/firmware/a300_pfp.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a300_pm4.fw:/system/etc/firmware/a300_pm4.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2S3D_adreno200.so:/system/lib/egl/libGLESv2S3D_adreno200.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/libC2D2.so:/system/lib/libC2D2.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/libgsl.so:/system/lib/libgsl.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/libOpenVG.so:/system/lib/libOpenVG.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so
+    $(VENDOR_DEVICE_FOLDER)/system/app/ProfilerPlaybackTools.apk:system/app/ProfilerPlaybackTools.apk \
+    $(VENDOR_DEVICE_FOLDER)/system/bin/gpu_dcvsd:system/bin/gpu_dcvsd \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libC2D2.so:system/lib/libC2D2.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libgsl.so:system/lib/libgsl.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
