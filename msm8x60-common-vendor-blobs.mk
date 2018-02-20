022f44f1c8d64b17ab91edf72d55d386064c5e01
Alexander Lam <lambchop468@gmail.com>
2018-02-19 19:54:20 -0500

    Revert "Update to New Adreno Blobs"
    This reverts commit 0c85765132e16fe85e9d065368689cba04354d6f.
    
    I'm giving up on this for now.

--- msm8x60-common-vendor-blobs.mk
+++ msm8x60-common-vendor-blobs.mk
@@ -41,10 +41,11 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     $(VENDOR_DEVICE_FOLDER)/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
     $(VENDOR_DEVICE_FOLDER)/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libEGL_adreno.so:system/lib/egl/libEGL_adreno.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv1_CM_adreno.so:system/lib/egl/libGLESv1_CM_adreno.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2_adreno.so:system/lib/egl/libGLESv2_adreno.so \
-    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libq3dtools_adreno.so:system/lib/egl/libq3dtools_adreno.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libC2D2.so:system/lib/libC2D2.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libgsl.so:system/lib/libgsl.so \
