f06cf13ab64a51072b9c7eca45eba925db902aea
Alexander Lam <lambchop468@gmail.com>
2018-02-24 14:31:20 -0500

    Update makefile to install missing files
    Not entirely sure if these libraries are actually needed, but it doesn't hurt to have them.

--- msm8x60-common-vendor-blobs.mk
+++ msm8x60-common-vendor-blobs.mk
@@ -44,9 +44,13 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libEGL_adreno.so:system/lib/egl/libEGL_adreno.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv1_CM_adreno.so:system/lib/egl/libGLESv1_CM_adreno.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2_adreno.so:system/lib/egl/libGLESv2_adreno.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libGLESv2S3D_adreno.so:system/lib/egl/libGLESv2S3D_adreno.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/egl/libq3dtools_adreno.so:system/lib/egl/libq3dtools_adreno.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libadreno_utils.so:system/lib/libadreno_utils.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libC2D2.so:system/lib/libC2D2.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libgsl.so:system/lib/libgsl.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
+    $(VENDOR_DEVICE_FOLDER)/system/lib/libOpenCL.so:system/lib/libOpenCL.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
     $(VENDOR_DEVICE_FOLDER)/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
